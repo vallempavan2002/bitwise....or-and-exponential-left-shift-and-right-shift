119df22cd10d0376f4816d3b271004b3cf9d485f
Pavan kalyan Vallem <116752882+vallempavan2002@users.noreply.github.com>
2023-03-01 15:04:39 +0530

    Create bitwise.java

---
+++ bitwise.java
@@ -0,0 +1,14 @@
+//bitwis operatore;   or oiperator
+public class Main 
+{
+public static void main(String[]args) 
+{
+    System.out.println(5&8);
+    System.out.println(10|16);
+     System.out.println(10^16);
+    System.out.println(10<<16);
+    System.out.println(10>>16);
+}
+}
+
+
